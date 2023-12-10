ebb56d9d10176477393c9a6e4e2d8763bf487bab
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-10 12:42:59 -0300

    feat: Criar classe BaseDecimalType para criação de tipos decimais SQL
    Cria a classe BaseDecimalType no pacote base.
    
    Ela é uma classe abstrata usada para implementar tipos decimais SQL concretos ou abstratos.

---
+++ src/sqlgen/types/base/base_decimal_type.py
@@ -0,0 +1,27 @@
+from typing import Any
+from . import BaseType
+from sqlgen.types.exceptions.base_decimal_type import InvalidPrecision
+
+
+class BaseDecimalType(BaseType):
+    def __init__(self, sql_type_name: str, length: int | None, precision: int | None) -> None:
+        super().__init__(sql_type_name, length)
+
+        self._validate_precision(precision)
+        self._precision = precision
+
+    def _validate_precision(self, precision: int) -> None:
+        if not self._is_precision_valid(precision):
+            raise InvalidPrecision(super().name)
+
+    def _is_precision_valid(self, precision: int) -> bool:
+        return (not super().length and not precision) or \
+            (super().length and not precision) or \
+            (isinstance(precision, int) and precision >= 0 and precision < super().length)
+
+    def _is_length_valid(self, length: int) -> bool:
+        return length is None or (isinstance(length, int) and length > 0)
+
+    @property
+    def precision(self) -> int:
+        return self._precision
