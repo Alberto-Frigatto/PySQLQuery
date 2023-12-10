e89450d0c3e401057812b59b84cd07c2a8bbceea
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-10 12:37:21 -0300

    feat: Adicionar exceções para BaseType
    Adicionadas no pacote types.exceptions exceções relacionadas às validações que a classe BaseType realiza.

---
+++ src/sqlgen/types/exceptions/base_type.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta
+
+
+class SqlBaseTypeException(Exception, metaclass=ABCMeta):
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+
+
+class InvalidTypeName(SqlBaseTypeException):
+    MESSAGE = 'Nome de tipo inválido'
+
+    def __init__(self) -> None:
+        super().__init__(self.MESSAGE)
+
+
+class InvalidTypeLenght(SqlBaseTypeException):
+    MESSAGE = 'Tamanho do tipo {type} é inválido'
+
+    def __init__(self, type_name: str) -> None:
+        super().__init__(self.MESSAGE.format(type=type_name))
+
+
+class InvalidValue(SqlBaseTypeException):
+    MESSAGE = 'O valor para o campo {type} é inválido'
+
+    def __init__(self, type_name: str) -> None:
+        super().__init__(self.MESSAGE.format(type=type_name))
