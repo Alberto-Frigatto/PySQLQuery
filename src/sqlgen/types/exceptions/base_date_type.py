b165c9f0c0e49943aa0329405ea19d81d81cc471
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-11 14:14:44 -0300

    docs: Adicionar docstring às exceções de BaseDateType
    Adiciona docstrings detalhadas às exceções de BaseDateType e ao módulo para melhorar a documentação do código.

--- src/sqlgen/types/exceptions/base_date_type.py
+++ src/sqlgen/types/exceptions/base_date_type.py
@@ -1,12 +1,35 @@
+'''
+Defines the base exception classes for date SQL type classes
+'''
+
 from .base_type import SqlBaseTypeException
 
 class SqlBaseDateTypeException(SqlBaseTypeException):
+    '''
+    Abstract base exception class for date SQL type-related exceptions.
+    '''
+
     def __init__(self, message: str) -> None:
         super().__init__(message)
 
 
 class InvalidDatePattern(SqlBaseTypeException):
+    '''
+    Exception raised for an invalid date SQL type pattern.
+    '''
+
     MESSAGE = 'O padrão para o tipo {type} é inválido'
 
     def __init__(self, type_name: str) -> None:
+        '''
+        Parameters
+        ----------
+        type_name : str
+            The name of SQL type.
+
+        Returns
+        -------
+        None
+        '''
+
         super().__init__(self.MESSAGE.format(type=type_name))
