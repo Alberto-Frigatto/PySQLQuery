f3e8781beac0e4941be009b49c89635d49c04da5
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-11 14:08:04 -0300

    docs: Adicionar docstring às exceções de BaseType
    Adiciona docstrings detalhadas às exceções de BaseType e ao módulo para melhorar a documentação do código.

--- src/sqlgen/types/exceptions/base_type.py
+++ src/sqlgen/types/exceptions/base_type.py
@@ -1,12 +1,24 @@
+'''
+Defines the base exception classes for SQL type classes
+'''
+
 from abc import ABCMeta
 
 
 class SqlBaseTypeException(Exception, metaclass=ABCMeta):
+    '''
+    Abstract base exception class for SQL type-related exceptions.
+    '''
+
     def __init__(self, message: str) -> None:
         super().__init__(message)
 
 
 class InvalidTypeName(SqlBaseTypeException):
+    '''
+    Exception raised for an invalid SQL type name.
+    '''
+
     MESSAGE = 'Nome de tipo inválido'
 
     def __init__(self) -> None:
@@ -14,14 +26,44 @@ class InvalidTypeName(SqlBaseTypeException):
 
 
 class InvalidTypeLength(SqlBaseTypeException):
+    '''
+    Exception raised for an invalid SQL type length.
+    '''
+
     MESSAGE = 'Tamanho do tipo {type} é inválido'
 
     def __init__(self, type_name: str) -> None:
+        '''
+        Parameters
+        ----------
+        type_name : str
+            The name of SQL type.
+
+        Returns
+        -------
+        None
+        '''
+
         super().__init__(self.MESSAGE.format(type=type_name))
 
 
 class InvalidValue(SqlBaseTypeException):
+    '''
+    Exception raised for an invalid SQL type value.
+    '''
+
     MESSAGE = 'O valor para o campo {type} é inválido'
 
     def __init__(self, type_name: str) -> None:
+        '''
+        Parameters
+        ----------
+        type_name : str
+            The name of SQL type.
+
+        Returns
+        -------
+        None
+        '''
+
         super().__init__(self.MESSAGE.format(type=type_name))
