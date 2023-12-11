a1a82566991313f1367ad937dfbcafc3927e2c92
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-11 14:12:06 -0300

    docs: Adicionar docstring às exceções de BaseDecimalType
    Adiciona docstrings detalhadas às exceções de BaseDecimalType e ao módulo para melhorar a documentação do código.

--- src/sqlgen/types/exceptions/base_decimal_type.py
+++ src/sqlgen/types/exceptions/base_decimal_type.py
@@ -1,12 +1,35 @@
+'''
+Defines the base exception classes for decimal SQL type classes
+'''
+
 from .base_type import SqlBaseTypeException
 
 class SqlBaseDecimalTypeException(SqlBaseTypeException):
+    '''
+    Abstract base exception class for decimal SQL type-related exceptions.
+    '''
+
     def __init__(self, message: str) -> None:
         super().__init__(message)
 
 
 class InvalidPrecision(SqlBaseTypeException):
+    '''
+    Exception raised for an invalid decimal SQL type precision.
+    '''
+
     MESSAGE = 'A precisão para o tipo {type} é inválido'
 
     def __init__(self, type_name: str) -> None:
+        '''
+        Parameters
+        ----------
+        type_name : str
+            The name of SQL type.
+
+        Returns
+        -------
+        None
+        '''
+
         super().__init__(self.MESSAGE.format(type=type_name))
