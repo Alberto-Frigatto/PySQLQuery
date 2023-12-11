fc9d3932781ff202d1c2cc070dec5cfdfcaf961c
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-10 22:07:44 -0300

    fix: Corrigida implementação da classe Char
    Permite agora o atributo _length ser None. Isso implica na renderização do tipo, que agora pode ser renderizado sem um tamanho especificado, e também na validação de dados, pois, se o length não é passado, o length usado na validação passa a ser 1.

--- src/sqlgen/types/char.py
+++ src/sqlgen/types/char.py
@@ -4,11 +4,8 @@ from .base import BaseType
 class Char(BaseType):
     _TYPE_NAME = 'char'
 
-    def __init__(self, length: int = 1) -> None:
+    def __init__(self, length: int | None = None) -> None:
         super().__init__(self._TYPE_NAME, length)
-
-    def _is_length_valid(self, length: int) -> bool:
-        return isinstance(length, int) and length > 0
 
     def __str__(self) -> str:
         rendered_value = super().name
@@ -19,4 +16,4 @@ class Char(BaseType):
         return rendered_value
 
     def validate_value(self, value: str) -> bool:
-        return isinstance(value, str) and len(value) <= super().length
+        return isinstance(value, str) and len(value) <= (super().length if super().length else 1)
