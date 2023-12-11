3209c9945a5e818b4bde7c3aa706fba809851e68
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-11 13:48:22 -0300

    docs: Adicionar docstring à classe BaseType
    Adiciona docstrings detalhadas à classe BaseType e ao módulo para melhorar a documentação do código.

--- src/sqlgen/types/base/base_type.py
+++ src/sqlgen/types/base/base_type.py
@@ -1,10 +1,36 @@
+'''
+Defines the abstract base class for constructing SQL type classes.
+'''
+
 from abc import ABCMeta, abstractmethod
 from typing import Any
 from sqlgen.types.exceptions.base_type import InvalidTypeName, InvalidTypeLength
 
 
 class BaseType(metaclass=ABCMeta):
+    '''
+    Abstract class for construct SQL type classes.
+
+    This class provides the basic structures for construct
+    concrete classes that represents SQL types.
+
+    This class must be inherited by concrete or another abstract one.
+    '''
+
     def __init__(self, sql_type_name: str, length: int | None) -> None:
+        '''
+        Parameters
+        ----------
+        sql_type_name : str
+            The name of SQL type.
+        length : int | None
+            The length of SQL type.
+
+        Returns
+        -------
+        None
+        '''
+
         super().__init__()
 
         self._validate_name(sql_type_name)
@@ -32,10 +58,29 @@ class BaseType(metaclass=ABCMeta):
 
     @abstractmethod
     def __str__(self) -> str:
+        '''
+        Returns
+        -------
+        str
+            A string representation of the class instance in SQL format.
+        '''
+
         pass
 
     @abstractmethod
     def validate_value(self, value: Any) -> bool:
+        '''
+        Parameters
+        ----------
+        value : Any
+            The value to be validated.
+
+        Returns
+        -------
+        bool
+            True if the value is valid for the SQL type, False otherwise.
+        '''
+
         pass
 
     @property
