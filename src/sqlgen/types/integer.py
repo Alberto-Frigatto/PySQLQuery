e5cd19e21abe30e80b60e5ed4c9d8c95a94a5737
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-11 21:18:12 -0300

    docs: Adicionar docstring à classe Integer
    Adiciona docstrings detalhadas à classe Integer e ao módulo para melhorar a documentação do código.

--- src/sqlgen/types/integer.py
+++ src/sqlgen/types/integer.py
@@ -1,10 +1,42 @@
+'''
+Defines the Integer class for constructing INTEGER SQL type.
+'''
+
 from .base import BaseType
 
 
 class Integer(BaseType):
+    '''
+    Represents a INTEGER data type in SQL.
+
+    This class inherits from `BaseType` and provides functionality
+    specific to the INTEGER data type.
+    '''
+
     _TYPE_NAME = 'integer'
 
     def __init__(self, length: int | None = None) -> None:
+        '''
+        Parameters
+        ----------
+        length : int | None
+            The length of INTEGER type.
+
+        Returns
+        -------
+        None
+
+        Examples
+        --------
+        >>> int_type = Integer()
+        >>> print(int_type)
+        INTEGER
+
+        >>> int_type = Integer(4)
+        >>> print(int_type)
+        INTEGER(4)
+        '''
+
         super().__init__(self._TYPE_NAME, length)
 
     def __str__(self) -> str:
@@ -16,4 +48,34 @@ class Integer(BaseType):
         return rendered_value
 
     def validate_value(self, value: int) -> bool:
+        '''
+        Parameters
+        ----------
+        value : int
+            The value to be validated.
+
+        Returns
+        -------
+        bool
+            True if the value is valid for the INTEGER SQL type, False otherwise.
+
+        Examples
+        --------
+        >>> int_type = Integer()
+        >>> int_type.validate_value(10)
+        True
+        >>> int_type.validate_value(10000000000000)
+        True
+        >>> int_type.validate_value(15.45)
+        False
+
+        >>> int_type = Integer(2)
+        >>> int_type.validate_value(10)
+        True
+        >>> int_type.validate_value(100)
+        False
+        >>> int_type.validate_value(1.5)
+        False
+        '''
+
         return isinstance(value, int) and (len(str(value)) <= super().length if super().length else True)
