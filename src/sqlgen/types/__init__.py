a0de23b035c9f147917eb144af2ce7309a822ffb
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-11 21:26:34 -0300

    docs: Adicionar docstring ao pacote types
    Adicionar docstring no arquivo __init__.py do pacote types.

--- src/sqlgen/types/__init__.py
+++ src/sqlgen/types/__init__.py
@@ -1,3 +1,16 @@
+'''
+Package for SQL type classes
+
+There are these classes:
+
+- `Char`
+- `Date`
+- `DateTime`
+- `Float`
+- `Integer`
+- `String`
+'''
+
 from .string import String
 from .float import Float
 from .integer import Integer
