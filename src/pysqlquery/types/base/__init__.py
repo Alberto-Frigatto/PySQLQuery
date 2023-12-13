7f20463e5a845c1930710c5da1b848cba628dc3b
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-13 11:45:44 -0300

    chore: Trocar imports para as classes base tipo no pacote base
    Troca os imports para as classes SQLType, SQLDateType e SQLDecimalType no arquivo __init__.py no pacote base.

--- src/pysqlquery/types/base/__init__.py
+++ src/pysqlquery/types/base/__init__.py
@@ -3,6 +3,6 @@ Package for abstract SQL type base classes
 '''
 
 
-from .base_type import BaseType
-from .base_date_type import BaseDateType
-from .base_decimal_type import BaseDecimalType
+from .sql_type import SQLType
+from .sql_date_type import SQLDateType
+from .sql_decimal_type import SQLDecimalType
