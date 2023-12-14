231de2788a409b7bc87db02d943dae35e70fe8f9
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-14 14:39:35 -0300

    refactor: Diminuir responsabilidades de SQLType
    Altera as docstrings deixando explícito que a classe deve ser herdada por outras classes abstratas.
    
    Retira todas as referências (validações e properties) ao atributo length, bem como ele próprio.

--- src/pysqlquery/types/base/sql_type.py
+++ src/pysqlquery/types/base/sql_type.py
@@ -1,31 +1,28 @@
 '''
-Defines the abstract base class for constructing SQL type classes.
+Defines the abstract base class for constructing abstract SQL type classes.
 '''
 
 from abc import ABCMeta, abstractmethod
 from typing import Any
-from pysqlquery.types.exceptions.sql_type import InvalidTypeName, InvalidTypeLength
+from pysqlquery.types.exceptions.sql_type import InvalidTypeName
 
 
 class SQLType(metaclass=ABCMeta):
     '''
-    Abstract class for construct SQL type classes.
+    Abstract class for construct abstract SQL type classes.
 
     This class provides the basic structures for construct
-    concrete classes that represents SQL types or another
-    abstract classes for other kind of SQL types.
+    abstract classes for kind of SQL types.
 
-    This class must be inherited by concrete or another abstract one.
+    This class must be inherited by abstract one.
     '''
 
-    def __init__(self, sql_type_name: str, length: int | None) -> None:
+    def __init__(self, sql_type_name: str) -> None:
         '''
         Parameters
         ----------
         sql_type_name : str
             The name of SQL type.
-        length : int | None
-            The length of SQL type.
 
         Returns
         -------
@@ -37,22 +34,12 @@ class SQLType(metaclass=ABCMeta):
         self._validate_name(sql_type_name)
         self._name: str = self._format_type_name(sql_type_name)
 
-        self._validate_length(length)
-        self._length: int | None = length
-
     def _validate_name(self, type: str) -> None:
         if not self._is_name_valid(type):
             raise InvalidTypeName()
 
     def _is_name_valid(self, type: str) -> bool:
         return isinstance(type, str) and len(type)
-
-    def _validate_length(self, length: int | None) -> None:
-        if not self._is_length_valid(length):
-            raise InvalidTypeLength(self._name)
-
-    def _is_length_valid(self, length: int | None) -> bool:
-        return length is None or (isinstance(length, int) and length > 0)
 
     def _format_type_name(self, type: str) -> str:
         return type.strip().upper()
@@ -87,7 +74,3 @@ class SQLType(metaclass=ABCMeta):
     @property
     def name(self) -> str:
         return self._name
-
-    @property
-    def length(self) -> int:
-        return self._length
