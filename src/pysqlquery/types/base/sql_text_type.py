2d865c0dac8a4806fd3696329bc9f43fe3a79db9
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-14 14:46:33 -0300

    feat: Criar classe SQLTextType no pacote base
    Cria a classe SQLTextType no pacote types.base.
    
    Ela é uma classe abstrata usada para implementar tipos SQL textuais concretos ou abstratos.

---
+++ src/pysqlquery/types/base/sql_text_type.py
@@ -0,0 +1,19 @@
+'''
+Defines the abstract base class for constructing text SQL type classes.
+'''
+
+from abc import ABCMeta
+from .sized_sql_type import SizedSQLType
+
+
+class SQLTextType(SizedSQLType, metaclass=ABCMeta):
+    '''
+    Abstract class for construct text SQL type classes.
+
+    This class inherits from `SizedSQLType` and provides the basic structures for construct
+    concrete classes that represents text SQL types or another
+    abstract classes for other kind of text SQL types.
+
+    This class must be inherited by concrete or another abstract one.
+    '''
+    pass
