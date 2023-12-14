45d98fa1195469cdb98d9ab73fbfdf908b43e9dc
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-14 14:57:18 -0300

    chore: Adicionados imports ao pacote types.base
    Adicionados ao arquivo __init__.py do pacote types.base imports para as classes SizedSQLType, SQLTextType, SQLNumType e SQLIntType.

--- src/pysqlquery/types/base/__init__.py
+++ src/pysqlquery/types/base/__init__.py
@@ -5,4 +5,8 @@ Package for abstract SQL type base classes
 
 from .sql_type import SQLType
 from .sql_date_type import SQLDateType
+from .sized_sql_type import SizedSQLType
+from .sql_text_type import SQLTextType
+from .sql_num_type import SQLNumType
+from .sql_int_type import SQLIntType
 from .sql_decimal_type import SQLDecimalType
