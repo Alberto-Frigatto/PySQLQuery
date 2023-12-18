9ea4d4debada0a978cb05c19bfa92067b27091a2
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-18 11:08:59 -0300

    refactor: Refatorar exceção InvalidPrecision
    Traduz a mensagem de erro e o construtor pede um parâmetro para incorporar à mensagem a precisão do tipo que foi inválida.

--- src/pysqlquery/types/exceptions/sql_decimal_type.py
+++ src/pysqlquery/types/exceptions/sql_decimal_type.py
@@ -3,6 +3,7 @@ Defines the base exception classes for decimal SQL type classes
 '''
 
 from abc import ABCMeta
+from typing import Any
 from .sql_type import SQLTypeException
 
 class SQLDecimalTypeException(SQLTypeException, metaclass=ABCMeta):
@@ -19,18 +20,20 @@ class InvalidPrecision(SQLTypeException):
     Exception raised for an invalid decimal SQL type precision.
     '''
 
-    MESSAGE = 'A precisão para o tipo {type} é inválido'
+    MESSAGE = 'The given precision of {type} type is invalid: {precision!r}'
 
-    def __init__(self, type_name: str) -> None:
+    def __init__(self, type_name: str, precision: Any) -> None:
         '''
         Parameters
         ----------
         type_name : str
             The name of SQL type.
+        precision : Any
+            The precision of SQL type.
 
         Returns
         -------
         None
         '''
 
-        super().__init__(self.MESSAGE.format(type=type_name))
+        super().__init__(self.MESSAGE.format(type=type_name, precision=precision))
