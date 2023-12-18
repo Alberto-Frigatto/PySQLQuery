1e95f04af74562e82d7b5c349024237e3a94d823
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-18 10:09:19 -0300

    refactor: Traduzir mensagem de InvalidDatePattern e pedir novo parâmetro
    Traduz a mensagem de erro e também pede um parâmetro date_patter para incorporar à mensagem o padrão inválido.

--- src/pysqlquery/types/exceptions/sql_date_type.py
+++ src/pysqlquery/types/exceptions/sql_date_type.py
@@ -19,18 +19,20 @@ class InvalidDatePattern(SQLTypeException):
     Exception raised for an invalid date SQL type pattern.
     '''
 
-    MESSAGE = 'O padrão para o tipo {type} é inválido'
+    MESSAGE = 'The given date pattern for the {type} type is invalid: {pattern!r}'
 
-    def __init__(self, type_name: str) -> None:
+    def __init__(self, type_name: str, date_pattern: str) -> None:
         '''
         Parameters
         ----------
         type_name : str
             The name of SQL type.
+        pattern : str
+            The date pattern of date SQL type.
 
         Returns
         -------
         None
         '''
 
-        super().__init__(self.MESSAGE.format(type=type_name))
+        super().__init__(self.MESSAGE.format(type=type_name, pattern=date_pattern))
