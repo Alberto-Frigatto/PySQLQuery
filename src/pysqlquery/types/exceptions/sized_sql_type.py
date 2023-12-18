701a459f7771fe5ec7a912deb0b80eef89b363f5
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-18 11:02:13 -0300

    refactor: Refatorar exceção InvalidTypeLength
    Traduz a mensagem de erro e o construtor pede um parâmetro para incorporar à mensagem o comprimento do tipo que foi inválido.

--- src/pysqlquery/types/exceptions/sized_sql_type.py
+++ src/pysqlquery/types/exceptions/sized_sql_type.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta
+from typing import Any
 from .sql_type import SQLTypeException
 
 
@@ -16,18 +17,20 @@ class InvalidTypeLength(SizedSQLTypeException):
     Exception raised for an invalid SQL type length.
     '''
 
-    MESSAGE = 'Tamanho do tipo {type} é inválido'
+    MESSAGE = 'The given length of {type} type is invalid: {length!r}'
 
-    def __init__(self, type_name: str) -> None:
+    def __init__(self, type_name: str, length: Any) -> None:
         '''
         Parameters
         ----------
         type_name : str
             The name of SQL type.
+        length : Any
+            The length of SQL type.
 
         Returns
         -------
         None
         '''
 
-        super().__init__(self.MESSAGE.format(type=type_name))
+        super().__init__(self.MESSAGE.format(type=type_name, length=length))
