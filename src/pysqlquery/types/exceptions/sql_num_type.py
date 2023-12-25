6e19ce6a29025ad3727e526da12d524d9fb2c203
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2023-12-25 15:06:35 -0300

    feat: Adicionar exceção para SQLNumType
    Adicionada no pacote types.exceptions exceção relacionada à classe SQLNumType.

---
+++ src/pysqlquery/types/exceptions/sql_num_type.py
@@ -0,0 +1,14 @@
+'''
+Defines the base exception class for numeric SQL type classes
+'''
+
+from abc import ABCMeta
+from .sized_sql_type import SizedSQLTypeException
+
+
+class SQLNumTypeException(SizedSQLTypeException, metaclass=ABCMeta):
+    '''
+    Abstract base exception class for numeric SQL type-related exceptions.
+    '''
+
+    pass
