6f684c115d4a53339364870379a44a5341e500b9
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-02 15:24:50 -0300

    docs: Alterar docstring do pacote types
    Altera a docstring no arquivo __init__.py no pacote types para explicar melhor a função do pacote.

--- src/pysqlquery/types/__init__.py
+++ src/pysqlquery/types/__init__.py
@@ -1,14 +1,14 @@
 '''
-Package for SQL type classes
+Package for SQL type classes.
 
 There are these classes:
 
-- `Char`
-- `Date`
-- `DateTime`
-- `Float`
-- `Integer`
-- `String`
+- `Char` - equivalent to CHAR type
+- `Date` - equivalent to DATE type
+- `DateTime` - equivalent to DATETIME type
+- `Float` - equivalent to FLOAT type
+- `Integer` - equivalent to INTEGER or INT type
+- `String` - equivalent to VARCHAR type
 '''
 
 from .string import String
