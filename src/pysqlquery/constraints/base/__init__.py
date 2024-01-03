35cd1084582dad0a692b80eb10af3dcb41823538
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-02 21:37:33 -0300

    chore: Adicionar imports ao __init__.py do pacote constraints
    Adicionados imports para as classes:
    
    - Constraint
    - UnnamedConstraint
    - NamedConstraint
    - SingleColumnNamedConstraint
    - MultiColumnNamedConstraint

---
+++ src/pysqlquery/constraints/base/__init__.py
@@ -0,0 +1,5 @@
+from .constraint import Constraint
+from .unnamed_constraint import UnnamedConstraint
+from .named_constraint import NamedConstraint
+from .single_column_named_constraint import SingleColumnNamedConstraint
+from .multi_column_named_constraint import MultiColumnNamedConstraint
