dfc1ce3db8cb5b816679df5ec457e203a21b53aa
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-04 22:49:41 -0300

    docs: Adicionar docstring ao pacote constraints
    Adiciona docstring ao arquivo __init__.py do pacote constraints pra explicar a função do pacote e detalhar quais classes ele oferece para uso.

--- src/pysqlquery/constraints/__init__.py
+++ src/pysqlquery/constraints/__init__.py
@@ -1,3 +1,14 @@
+'''
+Package for SQL constraint classes.
+
+There are these classes:
+
+- `ForeignKey` - for unnamed FOREIGN KEY constraint
+- `ForeignKeyConstraint` - for named FOREIGN KEY constraint
+- `PrimaryKeyConstraint` - for named PRIMARY KEY constraint
+- `UniqueConstraint` - for named UNIQUE constraint
+'''
+
 from .unnamed import ForeignKey
 from .named import (
     ForeignKeyConstraint,
