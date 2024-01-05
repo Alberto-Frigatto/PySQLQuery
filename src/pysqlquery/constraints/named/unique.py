0ae92406436b21408841c08a48beaa1f147f5a32
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-05 16:24:13 -0300

    docs: Adicionar docstrings à classe UniqueConstraint
    Adiciona docstrings detalhadas à classe UniqueConstraint e ao módulo para melhorar a documentação do código.

--- src/pysqlquery/constraints/named/unique.py
+++ src/pysqlquery/constraints/named/unique.py
@@ -1,8 +1,38 @@
+'''
+Defines the UniqueConstraint class for constructing named UNIQUE SQL constraint.
+'''
+
 from ..base import SingleColumnNamedConstraint
 
 
 class UniqueConstraint(SingleColumnNamedConstraint):
+    '''
+    Represents a named UNIQUE constraint in SQL.
+
+    This class inherits from `SingleColumnNamedConstraint` and provides functionality
+    specific to the named UNIQUE constraint.
+    '''
+
     def __init__(self, name: str, column: str) -> None:
+        '''
+        Parameters
+        ----------
+        name : str
+            The constraint's name.
+        column : str
+            The column's name that this constraint belongs to.
+
+        Returns
+        -------
+        None
+
+        Examples
+        --------
+        >>> un_const = UniqueConstraint('un_table_column', 'column')
+        >>> print(un_const)
+        CONSTRAINT un_table_column UNIQUE (column)
+        '''
+
         super().__init__(name, column)
 
     def __str__(self) -> str:
