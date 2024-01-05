c1a1587b6c0fc95dc20e24d20792ad0f7bb11a87
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-04 22:52:36 -0300

    docs: Adicionar docstrings à classe Constraint
    Adiciona docstrings detalhadas à classe Constraint e ao módulo para melhorar a documentação do código.

--- src/pysqlquery/constraints/base/constraint.py
+++ src/pysqlquery/constraints/base/constraint.py
@@ -1,7 +1,27 @@
+'''
+Defines the abstract base class for constructing abstract SQL constraint classes.
+'''
+
 from abc import ABCMeta, abstractmethod
 
 
 class Constraint(metaclass=ABCMeta):
+    '''
+    Abstract class for construct abstract SQL constraint classes.
+
+    This class provides the basic structure for construct
+    abstract classes for kind of SQL constraints.
+
+    This class must be inherited by abstract one.
+    '''
+
     @abstractmethod
     def __str__(self) -> str:
+        '''
+        Returns
+        -------
+        str
+            A string representation of the class instance in SQL format.
+        '''
+
         pass
