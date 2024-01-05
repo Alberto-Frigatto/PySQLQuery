415ad182b5bf03acd8701384e798a6b4546776bb
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-05 16:00:38 -0300

    docs: Adicionar docstring à exceção da classe Constraint
    Adiciona docstrings detalhadas à exceção da classe Constraint e ao módulo para melhorar a documentação do código.

--- src/pysqlquery/constraints/exceptions/constraint.py
+++ src/pysqlquery/constraints/exceptions/constraint.py
@@ -1,6 +1,14 @@
+'''
+Defines the base exception classes for SQL constraint classes.
+'''
+
 from abc import ABCMeta
 
 
 class ConstraintException(Exception, metaclass=ABCMeta):
+    '''
+    Abstract base exception class for SQL constraint-related exceptions.
+    '''
+
     def __init__(self, message: str) -> None:
         super().__init__(message)
