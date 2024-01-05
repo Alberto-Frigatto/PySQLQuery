e034d5d0be446752c603216565f8f6c41390e092
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-05 16:01:55 -0300

    docs: Adicionar docstrings às exceções da classe UnnamedConstraint
    Adiciona docstrings detalhadas às exceções da classe UnnamedConstraint e ao módulo para melhorar a documentação do código.

--- src/pysqlquery/constraints/exceptions/unnamed_constraint.py
+++ src/pysqlquery/constraints/exceptions/unnamed_constraint.py
@@ -1,15 +1,38 @@
+'''
+Defines the base exception classes for unnamed SQL constraint classes.
+'''
+
 from abc import ABCMeta
 from typing import Any
 from .constraint import ConstraintException
 
 
 class UnnamedConstraintException(ConstraintException, metaclass=ABCMeta):
+    '''
+    Abstract base exception class for unnamed SQL constraint-related exceptions.
+    '''
+
     def __init__(self, message: str) -> None:
         super().__init__(message)
 
 
 class InvalidAddedColumnName(UnnamedConstraintException):
+    '''
+    Exception raised for an invalid added column name.
+    '''
+
     MESSAGE = 'Added a invalid column name in ForeignKey constraint: {column!r}'
 
     def __init__(self, column: Any) -> None:
+        '''
+        Parameters
+        ----------
+        column : Any
+            The invalid column name.
+
+        Returns
+        -------
+        None
+        '''
+
         super().__init__(self.MESSAGE.format(column=column))
