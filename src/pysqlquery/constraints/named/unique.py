76e5b014a85f1f7717fae645c7261bff3603f5b9
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-05 18:21:55 -0300

    docs: Alterar docstrings da classe UniqueConstraint
    Adiciona informações às docstrings da classe UniqueConstraint para explicar melhor para quê e como usar a classe.

--- src/pysqlquery/constraints/named/unique.py
+++ src/pysqlquery/constraints/named/unique.py
@@ -9,6 +9,8 @@ class UniqueConstraint(SingleColumnNamedConstraint):
     '''
     Represents a named UNIQUE constraint in SQL.
 
+    This class can be used in table's __constraints__ list.
+
     This class inherits from `SingleColumnNamedConstraint` and provides functionality
     specific to the named UNIQUE constraint.
     '''
@@ -31,6 +33,14 @@ class UniqueConstraint(SingleColumnNamedConstraint):
         >>> un_const = UniqueConstraint('un_table_column', 'column')
         >>> print(un_const)
         CONSTRAINT un_table_column UNIQUE (column)
+        >>>
+        >>> class MyTable(Table):
+        >>> ... my_column = Column(CHAR)
+        >>> ... __constraints__ = [UniqueConstraint('un_my_table_my_column', 'my_column')]
+        >>> ...
+        >>> my_table = MyTable()
+        >>> print(my_table.my_column.unique)
+        >>> True
         '''
 
         super().__init__(name, column)
