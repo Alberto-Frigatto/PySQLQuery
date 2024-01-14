837b8687f5778db279b63e3104fd30e892a2b55b
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-14 17:49:38 -0300

    chore: Modificar __init__.py do pacote types
    Adiciona imports para as novas classes de tipo, e modifica a docstring do módulo para contemplar as novas classes.

--- src/pysqlquery/types/__init__.py
+++ src/pysqlquery/types/__init__.py
@@ -3,12 +3,18 @@ Package for SQL type classes.
 
 There are these classes:
 
+- `Bit` - equivalent to BIT type
+- `Boolean` - equivalent to BOOLEAN type
 - `Char` - equivalent to CHAR type
 - `Date` - equivalent to DATE type
 - `DateTime` - equivalent to DATETIME type
+- `Decimal` - equivalent to DECIMAL type
+- `Double` - equivalent to DOUBLE type
 - `Float` - equivalent to FLOAT type
 - `Integer` - equivalent to INTEGER or INT type
+- `Real` - equivalent to REAL type
 - `String` - equivalent to VARCHAR type
+- `Time` - equivalent to TIME type
 '''
 
 from .string import String
@@ -17,3 +23,9 @@ from .integer import Integer
 from .char import Char
 from .date import Date
 from .datetime import DateTime
+from .boolean import Boolean
+from .bit import Bit
+from .real import Real
+from .time import Time
+from .double import Double
+from .decimal import Decimal
