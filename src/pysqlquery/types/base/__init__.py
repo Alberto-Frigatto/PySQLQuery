b187234ff968e933571b0e54105e2fc5207bfad7
Alberto Frigatto de Andrade Ferreira <albertofrigattoferreira@gmail.com>
2024-01-15 19:36:33 -0300

    chore: Remover import à classe SizedSQLType no pacote types.base

--- src/pysqlquery/types/base/__init__.py
+++ src/pysqlquery/types/base/__init__.py
@@ -2,9 +2,10 @@
 Package for abstract SQL type base classes.
 '''
 
+# isort: skip_file
+
 from .sql_type import SQLType
 from .sql_date_type import SQLDateType
-from .sized_sql_type import SizedSQLType
 from .sql_text_type import SQLTextType
 from .sql_num_type import SQLNumType
 from .sql_int_type import SQLIntType
